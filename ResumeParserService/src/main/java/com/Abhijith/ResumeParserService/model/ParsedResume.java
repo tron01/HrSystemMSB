18af1bad0d10ff70de827b8755e98ebe769dc8c0
tron01 <69385258+tron01@users.noreply.github.com>
2025-06-04 01:07:55 +0530

    ParsedResume model

--- ResumeParserService/src/main/java/com/Abhijith/ResumeParserService/model/ParsedResume.java
+++ ResumeParserService/src/main/java/com/Abhijith/ResumeParserService/model/ParsedResume.java
@@ -1,17 +1,38 @@
 package com.Abhijith.ResumeParserService.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
 @Document(collection = "parsed_resumes")
-@Setter
-@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ParsedResume {
 	@Id
 	private String id;
-	private String name;
+	
+	private String applicantName;
 	private String email;
-	private String extractedText;
+	private String jobId;
+	
+	private String name;
+	private String phone;
+	private String linkedin;
+	private String github;
+	
+	private List<String> skills;
+	private String workExperience;
+	private String education;
+	private List<Map<String, String>> educationDetails;
+	private String certifications;
+	private String projects;
+	private String languages;
+	
+	private LocalDateTime parsedAt;
 }
