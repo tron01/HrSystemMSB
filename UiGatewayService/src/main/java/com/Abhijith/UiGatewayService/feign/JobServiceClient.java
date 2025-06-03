37d0027c445dd32f08aa003abb8b33ff3321091f
tron01 <69385258+tron01@users.noreply.github.com>
2025-06-03 19:57:36 +0530

    JobServiceClient  added to Uigateway

---
+++ UiGatewayService/src/main/java/com/Abhijith/UiGatewayService/feign/JobServiceClient.java
@@ -0,0 +1,23 @@
+package com.Abhijith.UiGatewayService.feign;
+
+import com.Abhijith.UiGatewayService.dto.jobRequest;
+import com.Abhijith.UiGatewayService.model.Job;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import java.util.List;
+
+@FeignClient(name = "job-posting-service")
+public interface JobServiceClient {
+
+	@PostMapping("/jobs")
+	Job createJob(@RequestBody jobRequest jobRequest);
+
+	@GetMapping("/jobs")
+	List<Job> getAllJobs();
+	
+	@GetMapping("/jobs/{id}")
+	Job getJobById(@PathVariable("id") String id);
+}
---
+++ UiGatewayService/src/main/java/com/Abhijith/UiGatewayService/model/Job.java
@@ -0,0 +1,14 @@
+package com.Abhijith.UiGatewayService.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Job {
+	private String id;
+	private String title;
+	private String description;
+	private String location;
+	private String postedBy;
+}
