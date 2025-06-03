ab14ced11d8f35509e05bbbce4ca3e01ea1e436e
tron01 <69385258+tron01@users.noreply.github.com>
2025-06-03 20:03:52 +0530

    ApplicationServiceClient  added to Uigateway

---
+++ UiGatewayService/src/main/java/com/Abhijith/UiGatewayService/dto/ApplicationDto.java
@@ -0,0 +1,14 @@
+package com.Abhijith.UiGatewayService.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApplicationDto {
+	private String id;
+	private String jobId;
+	private String studentName;
+	private String email;
+	private String resumeUrl;
+}
---
+++ UiGatewayService/src/main/java/com/Abhijith/UiGatewayService/feign/ApplicationServiceClient.java
@@ -0,0 +1,32 @@
+package com.Abhijith.UiGatewayService.feign;
+
+import com.Abhijith.UiGatewayService.dto.ApplicationDto;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
+
+@FeignClient(name = "ApplicationService")
+public interface ApplicationServiceClient {
+
+	@PostMapping(path = "/applications",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	ApplicationDto createApplication(
+			@RequestParam("jobId") String jobId,
+			@RequestParam("studentName") String studentName,
+			@RequestParam("email") String email,
+			@RequestPart("resumeFile") MultipartFile resumeFile);
+	
+	@GetMapping("/applications")
+	List<ApplicationDto> getAllApplications();
+	
+	@GetMapping("/applications/{id}")
+	ApplicationDto getApplicationById(@PathVariable("id") String id);
+	
+	@GetMapping("/applications/{id}/resume-url")
+	Map<String, String> getResumeUrl(@PathVariable("id") String id);
+	
+}
