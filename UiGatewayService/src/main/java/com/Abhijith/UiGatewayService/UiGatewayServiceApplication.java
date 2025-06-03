8ebbae7fa8923c36cde1691a13748ddd8ae50bb4
tron01 <69385258+tron01@users.noreply.github.com>
2025-06-03 19:55:55 +0530

    UigateWay added

---
+++ UiGatewayService/src/main/java/com/Abhijith/UiGatewayService/UiGatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package com.Abhijith.UiGatewayService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients(basePackages = "com.Abhijith.UiGatewayService.feign")
+public class UiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiGatewayServiceApplication.class, args);
+	}
+
+}
