b2c71c2d53cf91ec0d239417c3e5ebf6bf19d345
tron01 <69385258+tron01@users.noreply.github.com>
2025-06-04 01:30:16 +0530

    ResumeParserClient  added  UigatewayService

---
+++ UiGatewayService/src/main/java/com/Abhijith/UiGatewayService/feign/ResumeParserClient.java
@@ -0,0 +1,18 @@
+package com.Abhijith.UiGatewayService.feign;
+
+import com.Abhijith.UiGatewayService.dto.ParsedResume;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+
+@FeignClient(name = "resume-parser-service")
+public interface ResumeParserClient {
+
+	@GetMapping("/parsed-data")
+	List<ParsedResume> getAllParsedResumes();
+	
+	@GetMapping("/parsed-data/{id}")
+	ParsedResume getParsedResumeById(@PathVariable("id") String id);
+	
+}
