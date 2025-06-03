a6ba866369ee13fa42a8076d8a8986181a460d04
tron01 <69385258+tron01@users.noreply.github.com>
2025-06-03 19:58:15 +0530

    jobController added

---
+++ UiGatewayService/src/main/java/com/Abhijith/UiGatewayService/controller/JobController.java
@@ -0,0 +1,49 @@
+package com.Abhijith.UiGatewayService.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.Abhijith.UiGatewayService.feign.JobServiceClient;
+import com.Abhijith.UiGatewayService.model.Job;
+import com.Abhijith.UiGatewayService.dto.jobRequest;
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/v1/jobs")
+public class JobController {
+
+    @Autowired
+    private JobServiceClient jobServiceClient;
+
+    @GetMapping("")
+    public String listJobs(Model model) {
+        List<Job> jobs = jobServiceClient.getAllJobs();
+        model.addAttribute("jobs", jobs);
+        return "jobs";
+    }
+
+    @GetMapping("/new")
+    public String showCreateJobForm(Model model) {
+        model.addAttribute("jobRequest", new jobRequest());
+        return "job_form";
+    }
+
+    @PostMapping("")
+    public String createJob(@ModelAttribute jobRequest jobRequest) {
+        jobServiceClient.createJob(jobRequest);
+        return "redirect:/api/v1/jobs";
+    }
+
+    @GetMapping("/{id}")
+    public String getJobById(@PathVariable("id") String id, Model model) {
+        Job job = jobServiceClient.getJobById(id);
+        model.addAttribute("job", job);
+        return "job";
+    }
+}
