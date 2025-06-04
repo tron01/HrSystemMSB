efaee831c627c60a3835148450ebbbd551bfca1a
tron01 <69385258+tron01@users.noreply.github.com>
2025-06-04 19:48:45 +0530

    Update UiGatewayServiceApplication.java

--- UiGatewayService/src/main/java/com/Abhijith/UiGatewayService/UiGatewayServiceApplication.java
+++ UiGatewayService/src/main/java/com/Abhijith/UiGatewayService/UiGatewayServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "com.Abhijith.UiGatewayService.feign")
+@EnableFeignClients(basePackages = "com.Abhijith.UiGatewayService.client")
 public class UiGatewayServiceApplication {
 
 	public static void main(String[] args) {
